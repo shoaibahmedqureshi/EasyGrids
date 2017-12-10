eb0e55c024f21e6f788e5ca60b9caac438146a5a
Shoaib Ahmed Qureshi <shoaib.elastica@gmail.com>
2017-12-10 08:38:35 +0500

    Initial Commit

---
+++ EasyGrids/EasyGrids.h
@@ -0,0 +1,19 @@
+//
+//  EasyGrids.h
+//  EasyGrids
+//
+//  Created by Shoaib Ahmed Qureshi on 12/10/17.
+//  Copyright © 2017 Shoaib Ahmed Qureshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyGrids.
+FOUNDATION_EXPORT double EasyGridsVersionNumber;
+
+//! Project version string for EasyGrids.
+FOUNDATION_EXPORT const unsigned char EasyGridsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyGrids/PublicHeader.h>
+
+
---
+++ EasyGridsTests/EasyGridsTests.m
@@ -0,0 +1,39 @@
+//
+//  EasyGridsTests.m
+//  EasyGridsTests
+//
+//  Created by Shoaib Ahmed Qureshi on 12/10/17.
+//  Copyright © 2017 Shoaib Ahmed Qureshi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EasyGridsTests : XCTestCase
+
+@end
+
+@implementation EasyGridsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
